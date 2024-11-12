75d6859ad4fb1a883c9e7d3b2762b62e72974f45
vasu0200 <gollangivasudev@gmail.com>
2024-11-13 00:33:43 +0530

    latest footer changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import Logo from "../assets/images/Syntaxnow Logo.png"
 export default function Footer() {
   return (
     <div>
          <footer className="footer">
       <div className="footer-content">
         <div className="footer-column">
-          <div className="footer-logo">Logo</div>
+          <div className="footer-logo"><img className='logo' src={Logo} alt='' /></div>
           <ul>
             <li>About Us</li>
             <li>Our Approach</li>
@@ -21,37 +21,35 @@ export default function Footer() {
           </div>
           <p>info@syntaxnow.com</p>
         </div>
-
         <div className="footer-column">
           <h4>SERVICES</h4>
           <div className="services">
             <ul>
-              <li>Consulting</li>
+              <li><b>Consulting</b></li>
               <li>Product Discovery</li>
               <li>Solution Discovery</li>
               <li>UX / UI Design</li>
               <li>Technology Advisory</li>
             </ul>
             <ul>
-              <li>Cloud</li>
+              <li><b>Cloud</b></li>
               <li>Cloud Consulting</li>
               <li>Cloud Migration</li>
               <li>Cloud Development</li>
               <li>Kubernetes Services</li>
             </ul>
             <ul>
-              <li>Technology</li>
+              <li><b>Technology</b></li>
               <li>Big Data Engineering</li>
               <li>AI & Machine Learning</li>
             </ul>
             <ul>
-              <li>Teams</li>
+              <li><b>Teams</b></li>
               <li>Staff Augmentation</li>
               <li>Managed Team</li>
             </ul>
           </div>
         </div>
-
         <div className="footer-column">
           <h4>INDUSTRIES</h4>
           <ul>
@@ -64,8 +62,8 @@ export default function Footer() {
       <div className="footer-bottom">
         <p>© Copyright 2024 Syntaxnow. All Rights Reserved</p>
         <div className="footer-links">
-          <p>Privacy Policy</p>
-          <p>Terms of Use</p>
+          <p>Privacy Policy | </p>
+          <p>Terms of Use | </p>
           <p>Cookies Policy</p>
         </div>
       </div>
